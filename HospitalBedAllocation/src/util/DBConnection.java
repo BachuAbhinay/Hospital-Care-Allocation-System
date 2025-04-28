44ef3d4b8618c39d24e15ec9afb1433a26330baf
BachuAbhinay <abhinay.bachu@gmail.com>
2025-04-28 16:59:03 +0530

    Update DBConnection.java

--- HospitalBedAllocation/src/util/DBConnection.java
+++ HospitalBedAllocation/src/util/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:oracle:thin:@//Abhinay15:1522/xepdb1";
+    private static final String URL = "jdbc:oracle:thin:@//user15:1522/xepdb1";
     private static final String USER = "hospital_admin";
-    private static final String PASS = "hospital123";
+    private static final String PASS = "admin123";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
